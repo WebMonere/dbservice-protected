329991a9db086c1ef5e88b37aac33b9ab871a188
Rajdeep <rajdeepdas.india@gmail.com>
2019-10-14 07:48:18 +0530

    DB Service Protected Build Test Deploy

--- src/test/java/com/webmonere/dbservice/DbserviceApplicationTests.java
+++ src/test/java/com/webmonere/dbservice/DbserviceApplicationTests.java
@@ -3,6 +3,8 @@ package com.webmonere.dbservice;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,13 +22,27 @@ public class DbserviceApplicationTests {
 	@Autowired
 	private WebTestClient webClient;
 
-	/*@Test
-	public void contextLoads() throws UnirestException {
-		HttpResponse<String> response = (HttpResponse<String>) Unirest.get("http://localhost/api/private")
-				.header("authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlFVTTBRekUwTWtSQk5qZEJNRE16TWtNMk0wSTNOamRDTkRCQlEwSkZOVU5GT0RSR1FqWXhSQSJ9.eyJpc3MiOiJodHRwczovL3JhamRlZXBkZXYuYXV0aDAuY29tLyIsInN1YiI6IkFRUUZQczZhbGhkbEdQWHkxaEhxNjZKWkExSnFkWXVEQGNsaWVudHMiLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdC8iLCJpYXQiOjE1NzA2NDU3NTcsImV4cCI6MTU3MDczMjE1NywiYXpwIjoiQVFRRlBzNmFsaGRsR1BYeTFoSHE2NkpaQTFKcWRZdUQiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.EGYQSorcFuUqRtacpW-MgAM_XnMPyRaiY_I26YbRnY1n_WiPcJ1Kn3uyEdFYGXXJmwzUZnecT6W6YNn2Vm40lkdJB66d8MAdiYxjCwlSwMqNAW4Nux9h15nbqPmn9XXww3YtHsfykDSk6BDuf_fO3yTRQb0V0E4_vyCsEkFtJE7mgJEBRwc2ggYIq-ePvO6tSWKCHJAL9tIkx_Ou1QPVu_RusOAd0w90sTYfL_Na19bMk4dj9hN-2lBkqr1gL7pIOrn0Ul4CdNLhtsepNV5OX2DjGsvcpVU-_0aQ6LIOV0kl9grw6U69fej3Bj-XGDQfMqMGZQUlNU3ZX-jdmvTItQ")
-				.asString();
+	private String auth_token;
 
-		System.out.println(response.getBody());
+	public DbserviceApplicationTests() throws UnirestException {
+		HttpResponse<String> response = Unirest.post("https://rajdeepdev.auth0.com/oauth/token")
+				.header("content-type", "application/json")
+				.body("{\"client_id\":\"AQQFPs6alhdlGPXy1hHq66JZA1JqdYuD\",\"client_secret\":\"tj3hdzE1KIWE6uXBTtLHSrvj1j7OqNWNkC5Qzzr2IFAf5w7wzqJH_aDdCqveidvU\",\"audience\":\"https://localhost/\",\"grant_type\":\"client_credentials\"}")
+				.asString();
+		JSONObject temp1 = new JSONObject(response.getBody());
+		System.out.println(temp1.get("access_token"));
+		this.auth_token=temp1.get("access_token").toString();
+	}
+
+/*	@Test
+	public void contextLoads() throws UnirestException {
+		HttpResponse<String> response = Unirest.post("https://rajdeepdev.auth0.com/oauth/token")
+				.header("content-type", "application/json")
+				.body("{\"client_id\":\"AQQFPs6alhdlGPXy1hHq66JZA1JqdYuD\",\"client_secret\":\"tj3hdzE1KIWE6uXBTtLHSrvj1j7OqNWNkC5Qzzr2IFAf5w7wzqJH_aDdCqveidvU\",\"audience\":\"https://localhost/\",\"grant_type\":\"client_credentials\"}")
+			    .asString();
+		JSONObject temp1 = new JSONObject(response.getBody());
+		System.out.println(temp1.get("access_token"));
+		this.auth_token=temp1.get("access_token").toString();
 	}*/
 
 	//Check http://localhost/ has public access
@@ -43,7 +59,7 @@ public class DbserviceApplicationTests {
 	@Test
 	public void userResourceAuthAccessPermit() {
 		this.webClient.get().uri("/users")
-				.header("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlFVTTBRekUwTWtSQk5qZEJNRE16TWtNMk0wSTNOamRDTkRCQlEwSkZOVU5GT0RSR1FqWXhSQSJ9.eyJpc3MiOiJodHRwczovL3JhamRlZXBkZXYuYXV0aDAuY29tLyIsInN1YiI6IkFRUUZQczZhbGhkbEdQWHkxaEhxNjZKWkExSnFkWXVEQGNsaWVudHMiLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdC8iLCJpYXQiOjE1NzA2NDU3NTcsImV4cCI6MTU3MDczMjE1NywiYXpwIjoiQVFRRlBzNmFsaGRsR1BYeTFoSHE2NkpaQTFKcWRZdUQiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.EGYQSorcFuUqRtacpW-MgAM_XnMPyRaiY_I26YbRnY1n_WiPcJ1Kn3uyEdFYGXXJmwzUZnecT6W6YNn2Vm40lkdJB66d8MAdiYxjCwlSwMqNAW4Nux9h15nbqPmn9XXww3YtHsfykDSk6BDuf_fO3yTRQb0V0E4_vyCsEkFtJE7mgJEBRwc2ggYIq-ePvO6tSWKCHJAL9tIkx_Ou1QPVu_RusOAd0w90sTYfL_Na19bMk4dj9hN-2lBkqr1gL7pIOrn0Ul4CdNLhtsepNV5OX2DjGsvcpVU-_0aQ6LIOV0kl9grw6U69fej3Bj-XGDQfMqMGZQUlNU3ZX-jdmvTItQ")
+				.header("Authorization", "Bearer"+" "+this.auth_token)
 				.exchange().expectStatus().isOk();
 	}
 	@Test
@@ -54,7 +70,7 @@ public class DbserviceApplicationTests {
 	@Test
 	public void HostNamesResourceAuthAccessPermit() {
 		this.webClient.get().uri("/hostnames")
-				.header("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlFVTTBRekUwTWtSQk5qZEJNRE16TWtNMk0wSTNOamRDTkRCQlEwSkZOVU5GT0RSR1FqWXhSQSJ9.eyJpc3MiOiJodHRwczovL3JhamRlZXBkZXYuYXV0aDAuY29tLyIsInN1YiI6IkFRUUZQczZhbGhkbEdQWHkxaEhxNjZKWkExSnFkWXVEQGNsaWVudHMiLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdC8iLCJpYXQiOjE1NzA2NDU3NTcsImV4cCI6MTU3MDczMjE1NywiYXpwIjoiQVFRRlBzNmFsaGRsR1BYeTFoSHE2NkpaQTFKcWRZdUQiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.EGYQSorcFuUqRtacpW-MgAM_XnMPyRaiY_I26YbRnY1n_WiPcJ1Kn3uyEdFYGXXJmwzUZnecT6W6YNn2Vm40lkdJB66d8MAdiYxjCwlSwMqNAW4Nux9h15nbqPmn9XXww3YtHsfykDSk6BDuf_fO3yTRQb0V0E4_vyCsEkFtJE7mgJEBRwc2ggYIq-ePvO6tSWKCHJAL9tIkx_Ou1QPVu_RusOAd0w90sTYfL_Na19bMk4dj9hN-2lBkqr1gL7pIOrn0Ul4CdNLhtsepNV5OX2DjGsvcpVU-_0aQ6LIOV0kl9grw6U69fej3Bj-XGDQfMqMGZQUlNU3ZX-jdmvTItQ")
+				.header("Authorization","Bearer"+" "+this.auth_token)
 				.exchange().expectStatus().isOk();
 	}
 	@Test
@@ -65,7 +81,7 @@ public class DbserviceApplicationTests {
 	@Test
 	public void HostMappingResourceAuthAccessPermit() {
 		this.webClient.get().uri("/hostmappings")
-				.header("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlFVTTBRekUwTWtSQk5qZEJNRE16TWtNMk0wSTNOamRDTkRCQlEwSkZOVU5GT0RSR1FqWXhSQSJ9.eyJpc3MiOiJodHRwczovL3JhamRlZXBkZXYuYXV0aDAuY29tLyIsInN1YiI6IkFRUUZQczZhbGhkbEdQWHkxaEhxNjZKWkExSnFkWXVEQGNsaWVudHMiLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdC8iLCJpYXQiOjE1NzA2NDU3NTcsImV4cCI6MTU3MDczMjE1NywiYXpwIjoiQVFRRlBzNmFsaGRsR1BYeTFoSHE2NkpaQTFKcWRZdUQiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.EGYQSorcFuUqRtacpW-MgAM_XnMPyRaiY_I26YbRnY1n_WiPcJ1Kn3uyEdFYGXXJmwzUZnecT6W6YNn2Vm40lkdJB66d8MAdiYxjCwlSwMqNAW4Nux9h15nbqPmn9XXww3YtHsfykDSk6BDuf_fO3yTRQb0V0E4_vyCsEkFtJE7mgJEBRwc2ggYIq-ePvO6tSWKCHJAL9tIkx_Ou1QPVu_RusOAd0w90sTYfL_Na19bMk4dj9hN-2lBkqr1gL7pIOrn0Ul4CdNLhtsepNV5OX2DjGsvcpVU-_0aQ6LIOV0kl9grw6U69fej3Bj-XGDQfMqMGZQUlNU3ZX-jdmvTItQ")
+				.header("Authorization","Bearer"+" "+this.auth_token)
 				.exchange().expectStatus().isOk();
 	}
 
